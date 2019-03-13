b3e216887da7b243335cedd5420587a0a4fb0bf2
Gieandes Silva <contato@gieandessilva.com>
2019-03-13 14:38:17 -0300

    Starting the project

---
+++ src/components/CompletedTasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="completed-tasks">
+        <p>Sua Vida está em dia :D</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <div v-for="(task, i) in tasks" :key="task.name">
+                <Task :task="task" @taskDeleted="$emit('taskDeleted', i)"  @alterStatusTask="$emit('alterStatusTask', i, $event)" />
+            </div>
+        </template>
+        <CompletedTasks v-else />
+    </div>
+</template>
+
+<script>
+import Task from '@/components/Task'
+import CompletedTasks from '@/components/CompletedTasks'
+export default {
+    components: { Task, CompletedTasks },
+    props: { tasks: Array, required: true }
+}
+</script>
+
+<style>
+
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .task-grid .task {
+        margin: 10px;
+    }
+    
+</style>
